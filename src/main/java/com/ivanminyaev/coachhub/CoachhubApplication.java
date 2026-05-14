c765fcd79874f0af3913b474ab578a1fb3aa8b07
Ivan Minyaev <i.onechesz@gmail.com>
2026-05-14 13:25:14 +0300

    Spring Initializr init

---
+++ src/main/java/com/ivanminyaev/coachhub/CoachhubApplication.java
@@ -0,0 +1,13 @@
+package com.ivanminyaev.coachhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivanminyaev/coachhub/CoachhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanminyaev.coachhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
